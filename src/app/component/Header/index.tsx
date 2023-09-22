0cd0a3f7fb3a2cd4308e5358e947da7a77dc5a14
ppotatoG <thgus2270@gmail.com>
2023-09-22 21:45:45 +0900

    Add: Header

---
+++ src/app/component/Header/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DUMMY_USER_NAME = '홍길동';
+
+const Navigation = () => {
+  return (
+    <header className="flex items-center justify-end gap-4 p-4 shadow-md shadow-gray-200 dark:shadow-gray-800">
+      <p>
+        안녕하세요, <b>{DUMMY_USER_NAME}</b>님
+      </p>
+      <button className="border border-solid border-gray-400 dark:border-gray-300 rounded-md px-2 py-1">
+        로그아웃
+      </button>
+    </header>
+  );
+};
+
+export default Navigation;
