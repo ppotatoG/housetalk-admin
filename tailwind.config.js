2f8a8c26bae0c4d8d27516ab8ac0d94f2166eb75
ppotatoG <thgus2270@gmail.com>
2023-09-22 21:45:08 +0900

    Feat: safelist 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,4 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {
-      colors: {
-        'nav-bg': '#282B4A',
-        'nav-text': '#fff',
-        'nav-sub-bg': '#D8D8D8',
-        'nav-sub-text': '#393939',
-      },
-    },
-  },
-  plugins: [],
+  safelist: ['pl-4', 'pl-8'],
 };
