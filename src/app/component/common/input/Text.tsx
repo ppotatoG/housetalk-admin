05cbb48979af3c1506428998a0b4635b25e5e2ae
ppotatoG <thgus2270@gmail.com>
2023-09-25 23:07:49 +0900

    Feat: Input > Text

---
+++ src/app/component/common/input/Text.tsx
@@ -0,0 +1,47 @@
+interface TextProps {
+  label: string;
+  value: string;
+  setValue: (value: string) => void;
+  labelText: string;
+  placeholder?: string;
+  type?: string;
+  required?: boolean;
+  disabled?: boolean;
+  className?: string;
+}
+
+const Text = ({
+  label,
+  value,
+  setValue,
+  labelText,
+  placeholder,
+  type,
+  required,
+  disabled,
+  className,
+}: TextProps) => {
+  return (
+    <div
+      className={`border border-gray-200 dark:border-gray-800 flex items-center w-1/2 ${
+        className || ''
+      }`}
+    >
+      <label htmlFor={label} className="bg-gray-200 dark:bg-gray-800 p-2 w-1/2">
+        {labelText}
+      </label>
+      <input
+        className="p-2 w-1/2"
+        type={type ? type : 'text'}
+        id={label}
+        value={value}
+        placeholder={placeholder || undefined}
+        disabled={disabled || undefined}
+        onChange={e => setValue(e.target.value)}
+        {...(required ? { required: true } : {})}
+      />
+    </div>
+  );
+};
+
+export default Text;
