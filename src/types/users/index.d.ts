46b94c4bf4d60a94173378d24c1efdfd8d880a0d
ppotatoG <thgus2270@gmail.com>
2023-10-04 23:57:00 +0900

    Feat: Generate dummy users data

---
+++ src/types/users/index.d.ts
@@ -0,0 +1,42 @@
+interface User {
+  id: string;
+  password: string;
+  signUpMethod: 'naver' | 'kakao' | 'email';
+  pushNotification: boolean;
+  nickname: string;
+  name: string;
+  birthDate: string;
+  phone: string;
+  gender: gender;
+  signUpDate: string;
+  address: string;
+  livingInfo: LivingInfo;
+  postHistory: Post[];
+  reportHistory: Report[];
+}
+
+interface LivingInfo {
+  duration: '1-2 years' | '3-4 years' | '5 years or more';
+  type: 'One-room' | 'Two-room';
+  people: '1 person' | '2 people' | '3 or more people';
+}
+
+interface Post {
+  postId: string;
+  content: string;
+  datePosted: string;
+}
+
+interface Report {
+  reportId: string;
+  reason: string;
+  dateReported: string;
+}
+
+interface searchUser {
+  id: string;
+  name: string;
+  gender: gender;
+  nickname: string;
+  signUpDate: string;
+}
