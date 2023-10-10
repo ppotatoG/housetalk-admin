ea5dc7c1866d6325f362a10d6db7279d18fb9fe5
ppotatoG <thgus2270@gmail.com>
2023-10-11 02:09:30 +0900

    Feat: add 404 Not Found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import Button from '@/component/common/Button';
+const NotFound = () => {
+  const router = useRouter();
+
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center h-screen gap-4 text-center">
+        <h1 className="text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+          404
+        </h1>
+        <p className="text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+          Something's missing.
+        </p>
+        <p className="text-lg font-light text-gray-500 dark:text-gray-400">
+          Sorry, we can't find that page. You'll find lots to explore on the
+          home page.
+        </p>
+        <Button
+          onClick={router.back}
+          variant="filled"
+          className="ml-auto mr-auto"
+          text="Back to Homepage"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default NotFound;
