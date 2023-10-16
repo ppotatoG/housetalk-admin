f863bdf3f5ad1c4d045ff7ff7d00ac3c84519fc3
ppotatoG <thgus2270@gmail.com>
2023-10-17 01:19:32 +0900

    Style: Updated styling for the table component

--- src/app/component/common/table/index.tsx
+++ src/app/component/common/table/index.tsx
@@ -36,95 +36,92 @@ const Table: React.FC<TableProps> = ({
 
   return (
     <div className="mt-8 flex flex-col">
-      <div className="-m-1.5 overflow-x-auto">
-        <div className="p-1.5 min-w-full inline-block align-middle">
-          <div className="border rounded-lg overflow-hidden dark:border-gray-700">
-            <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-              <thead className="bg-gray-50 dark:bg-gray-700">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400"
-                  >
-                    NO
-                  </th>
-                  {headers.map((header, idx) => (
-                    <th
-                      key={idx}
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400"
+      <div className="border rounded-lg dark:border-gray-700">
+        <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+          <thead className="bg-gray-50 dark:bg-gray-700">
+            <tr>
+              <th
+                scope="col"
+                className="px-4 py-6 text-left font-normal text-gray-500 uppercase dark:text-gray-400 whitespace-no-wrap"
+              >
+                NO
+              </th>
+              {headers.map((header, idx) => (
+                <th
+                  key={idx}
+                  scope="col"
+                  className="px-4 py-6 text-left font-normal text-gray-500 uppercase dark:text-gray-400 whitespace-no-wrap"
+                >
+                  {header}
+                </th>
+              ))}
+              {onClickViewAction && (
+                <th
+                  scope="col"
+                  className="px-4 py-6 text-left font-normal text-gray-500 uppercase dark:text-gray-400 whitespace-no-wrap"
+                >
+                  상세
+                </th>
+              )}
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+            {currentRows.map((row, rowIndex) => (
+              <tr key={rowIndex}>
+                <td className="px-4 py-6 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                  {startIndex + rowIndex + 1}
+                </td>
+                {Object.entries(row).map(([key, value], cellIndex) => {
+                  if (hiddenColumns.includes(key)) return null;
+                  return (
+                    <td
+                      key={cellIndex}
+                      className="px-4 py-6 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"
                     >
-                      {header}
-                    </th>
-                  ))}
-                  {onClickViewAction && (
-                    <th
-                      scope="col"
-                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400"
-                    >
-                      상세
-                    </th>
-                  )}
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                {currentRows.map((row, rowIndex) => (
-                  <tr key={rowIndex}>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                      {startIndex + rowIndex + 1}
+                      {value.toString()}
                     </td>
-                    {Object.entries(row).map(([key, value], cellIndex) => {
-                      if (hiddenColumns.includes(key)) return null;
-                      return (
-                        <td
-                          key={cellIndex}
-                          className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200"
-                        >
-                          {value.toString()}
-                        </td>
-                      );
-                    })}
-                    {onClickViewAction && (
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                        <Button
-                          onClick={() => onClickViewAction(row)}
-                          type="button"
-                          variant="outlined"
-                        >
-                          보기
-                        </Button>
-                      </td>
+                  );
+                })}
+                {onClickViewAction && (
+                  <td className="px-4 py-6 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                    <Button
+                      onClick={() => onClickViewAction(row)}
+                      type="button"
+                      variant="outlined"
+                      size="small"
+                    >
+                      보기
+                    </Button>
+                  </td>
+                )}
+                {(onClickUpdateAction || onClickDeleteAction) && (
+                  <td className="px-4 py-6 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200 gap-1 flex ">
+                    {onClickUpdateAction && (
+                      <Button
+                        onClick={() => onClickUpdateAction(row)}
+                        type="button"
+                        variant="outlined"
+                        size="small"
+                      >
+                        수정
+                      </Button>
                     )}
-                    {(onClickUpdateAction || onClickDeleteAction) && (
-                      <td className="px-6 py-4 gap-2 flex whitespace-nowrap text-sm font-medium">
-                        {onClickUpdateAction && (
-                          <Button
-                            onClick={() => onClickUpdateAction(row)}
-                            type="button"
-                            variant="outlined"
-                            size="small"
-                          >
-                            수정
-                          </Button>
-                        )}
-                        {onClickDeleteAction && (
-                          <Button
-                            onClick={() => onClickDeleteAction(row)}
-                            type="button"
-                            variant="filled"
-                            size="small"
-                          >
-                            삭제
-                          </Button>
-                        )}
-                      </td>
+                    {onClickDeleteAction && (
+                      <Button
+                        onClick={() => onClickDeleteAction(row)}
+                        type="button"
+                        variant="filled"
+                        size="small"
+                      >
+                        삭제
+                      </Button>
                     )}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
       <Pagination
