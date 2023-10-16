488106cf1ef3b7c31e18e547f19b7158012e920b
ppotatoG <thgus2270@gmail.com>
2023-10-17 01:19:10 +0900

    Feat: Added data and functionality to the exhibition page

--- src/app/component/Exhibition/main/banner/index.tsx
+++ src/app/component/Exhibition/main/banner/index.tsx
@@ -8,41 +8,57 @@ import Button from '@/component/common/Button';
 import PageHeader from '@/component/common/header/PageHeader';
 import { RangeDate, Radio } from '@/component/common/input';
 import Table from '@/component/common/table';
-import { EXPOSURE_LIST, USAGE_STATUS } from '@/constants/exhibition';
+import {
+  EXHIBITION_TABLE_HEADER,
+  DUMMY_EXHIBITION,
+  USAGE_STATUS,
+  EXPOSURE_LIST,
+} from '@/constants/exhibition';
 import { PAGE_HEADERS } from '@/constants/pageHeaders';
 import { TABLE_ROW_LIMIT } from '@/constants/TABLE_ROW_LIMIT';
-import { DUMMY_USERS, USERS_TABLE_HEADER } from '@/constants/users';
 
 const BannerPage = () => {
   const [minValue, setMinValue] = useState<Date>(
     moment().subtract(6, 'months').toDate()
   );
   const [maxValue, setMaxValue] = useState<Date>(new Date());
-  const [users, setUsers] = useState<searchUser[] | null>(null);
+  const [exhibition, setExhibition] = useState<SearchExhibition[] | null>(null);
   const router = useRouter();
+
+  const publishStatus = (state: boolean) => (state ? '노출' : '미노출');
+  const usedStatus = (state: boolean) => (state ? '사용' : '미사용');
 
   const handelSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    setUsers(
-      DUMMY_USERS.map(user => ({
-        id: user.id,
-        name: user.name,
-        gender: user.gender,
-        nickname: user.nickname,
-        signUpDate: user.signUpDate,
-      }))
+    setExhibition(
+      DUMMY_EXHIBITION.map(exhibition => {
+        return {
+          imageUrl: exhibition.imageUrl,
+          isPublished: publishStatus(exhibition.isPublished),
+          displayDate: `${exhibition.displayStartDate} ~ ${exhibition.displayEndDate}`,
+          isUsed: usedStatus(exhibition.isUsed),
+          createdBy: exhibition.createdBy,
+          createdDate: exhibition.createdDate,
+          lastModifiedBy: exhibition.lastModifiedBy || '-',
+          id: exhibition.id,
+        };
+      })
     );
 
     console.log(minValue, maxValue);
   };
 
-  const onClickViewAction = (row: searchUser) => {
-    router.push(`users/${row.id}`);
+  const onClickUpdateAction = (row: Row) => {
+    router.push(`banner/update/${row.id}`);
+  };
+
+  const onClickDeleteAction = (row: Row) => {
+    console.log('onClickDeleteAction', row);
   };
 
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const totalPages = DUMMY_USERS.length / TABLE_ROW_LIMIT;
+  const totalPages = DUMMY_EXHIBITION.length / TABLE_ROW_LIMIT;
   const [exposure, setExposure] = useState<CheckData>(EXPOSURE_LIST[0]);
   const [usage, setUsage] = useState<CheckData>(USAGE_STATUS[0]);
 
@@ -80,22 +96,33 @@ const BannerPage = () => {
           radioList={USAGE_STATUS}
         />
 
-        <Button
-          variant="filled"
-          color="default"
-          text="검색"
-          padding="px-6 py-3"
-          type="submit"
-        />
+        <div className="flex justify-end gap-4">
+          <Button
+            variant="outlined"
+            color="default"
+            text="등록하기"
+            onClick={() => router.push('banner/create')}
+            className="!m-0"
+          />
+          <Button
+            variant="filled"
+            color="default"
+            text="검색"
+            type="submit"
+            className="!m-0"
+          />
+        </div>
       </form>
-      {users && (
+      {exhibition && (
         <Table
-          headers={USERS_TABLE_HEADER}
-          rows={users}
-          onClickViewAction={onClickViewAction}
+          headers={EXHIBITION_TABLE_HEADER}
+          rows={exhibition}
+          onClickUpdateAction={onClickUpdateAction}
+          onClickDeleteAction={onClickDeleteAction}
           currentPage={currentPage}
           totalPages={totalPages}
           onPageChange={handlePageChange}
+          hiddenColumns={['id']}
         />
       )}
     </div>
--- src/types/exhibition/index.d.ts
+++ src/types/exhibition/index.d.ts
@@ -1,3 +1,28 @@
+interface Exhibition {
+  id: string;
+  title: string;
+  description: string;
+  displayStartDate: string;
+  displayEndDate: string;
+  imageUrl: string;
+  isPublished: boolean;
+  isUsed: boolean;
+  createdBy: string;
+  createdDate: string;
+  lastModifiedBy: string;
+}
+
+interface SearchExhibition {
+  id: string;
+  imageUrl: string;
+  isPublished: string;
+  displayDate: string;
+  isUsed: string;
+  createdBy: string;
+  createdDate: string;
+  lastModifiedBy: string;
+}
+
 interface CheckData {
   id: string;
   label: string;
