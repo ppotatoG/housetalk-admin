edc52d0ee110d5ab7b286b2116d1be706686c3bc
ppotatoG <thgus2270@gmail.com>
2023-10-18 23:17:12 +0900

    Fix: resolve ESLint issue with single quote escaping

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,11 +13,11 @@ const NotFound = () => {
           404
         </h1>
         <p className="text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
-          Something's missing.
+          Something&apos;s missing.
         </p>
         <p className="text-lg font-light text-gray-500 dark:text-gray-400">
-          Sorry, we can't find that page. You'll find lots to explore on the
-          home page.
+          Sorry, we can&apos;t find that page. You&apos;ll find lots to explore
+          on the home page.
         </p>
         <Button
           onClick={router.back}
