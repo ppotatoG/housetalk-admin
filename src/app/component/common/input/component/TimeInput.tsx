b96ee066a5437a90137c3244d06fa0727d05dc21
ppotatoG <thgus2270@gmail.com>
2023-10-21 02:20:14 +0900

    Refactor: TimeInput

--- src/app/component/common/input/component/TimeInput.tsx
+++ src/app/component/common/input/component/TimeInput.tsx
@@ -6,7 +6,6 @@ interface TimeInputProps {
   label: string;
   value: number;
   setValue: (value: number) => void;
-  isHour?: boolean;
   timeType: 'HOUR' | 'MINUTE' | 'SECOND';
 }
 
@@ -14,32 +13,44 @@ const TimeInput: React.FC<TimeInputProps> = ({
   label,
   value,
   setValue,
-  isHour,
   timeType,
 }) => {
+  const maxNumber = timeType === 'HOUR' ? 23 : 59;
+
+  const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(Math.min(maxNumber, Math.max(0, Number(e.target.value))));
+  };
+
   const handleDelete = () => {
     setValue(0);
   };
 
   return (
-    <label htmlFor={label} className="flex border border-gray-200 w-20 pl-2">
-      <input
-        className="w-3/4"
-        type="text"
-        name=""
-        id={label}
-        value={value}
-        min={0}
-        max={isHour ? 23 : 59}
-        onChange={e => setValue(Number(e.target.value))}
-      />
-      <button className="w-1/4 text-gray-400">
-        <TiDelete onClick={handleDelete} />
-      </button>
-      <span className="border-l-2 border-gray-200 px-2">
+    <div className="flex border border-gray-200">
+      <label htmlFor={label} className="flex justify-between w-16">
+        <input
+          className="w-full block px-2"
+          type="number"
+          name={label}
+          id={label}
+          value={value}
+          min={0}
+          max={maxNumber}
+          onChange={handleValueChange}
+        />
+        <button
+          className="text-gray-400 pr-2"
+          type="button"
+          onClick={handleDelete}
+          aria-label="Delete"
+        >
+          <TiDelete />
+        </button>
+      </label>
+      <p className="border-l border-gray-200 bg-gray-50 text-gray-300 text-xs px-2">
         {TIME_TYPE_STRING[timeType]}
-      </span>
-    </label>
+      </p>
+    </div>
   );
 };
 
