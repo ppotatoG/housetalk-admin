fcc716e32cc5447858e0701985fb617d251c45cb
ppotatoG <thgus2270@gmail.com>
2023-10-23 00:02:38 +0900

    Fix: location is not defined error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-  return router.push('dashboard');
+  // TODO: auto token check
+  return redirect('/dashboard');
 }
