e08837542fc6c26a2b452bae76e6591b8ec5b3b9
ppotatoG <thgus2270@gmail.com>
2023-10-22 23:00:52 +0900

    Feat: auto focus on input during isEdit mode

--- src/app/component/common/input/ToggleEditableText.tsx
+++ src/app/component/common/input/ToggleEditableText.tsx
@@ -1,21 +1,25 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import Button from '@/component/common/Button';
 
 interface TextProps {
+  id: string;
   value: string;
   labelText: string;
   handleSave: () => void;
 }
 
 const ToggleEditableText: React.FC<TextProps> = ({
+  id,
   value,
   labelText,
   handleSave,
 }) => {
   const [isEdit, setIsEdit] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handleEdit = () => {
     if (isEdit) {
       handleSave();
@@ -25,22 +29,32 @@ const ToggleEditableText: React.FC<TextProps> = ({
     }
   };
 
+  useEffect(() => {
+    if (isEdit) {
+      inputRef.current?.focus();
+    }
+  }, [isEdit]);
+
   return (
     <div className="h-9 relative border border-gray-200 dark:border-gray-800 flex items-center">
       <div className="h-full leading-9 bg-gray-200 dark:bg-gray-800 px-2 w-1/3">
         {labelText}
       </div>
       <div className="h-full leading-9 px-2 w-2/3 flex justify-between items-center">
-        <label htmlFor={value} className="h-full py-1">
+        <label htmlFor={id} className="h-full py-1">
           <input
             className={`w-60 block h-full ${
               isEdit ? 'bg-indigo-50' : 'bg-transparent'
             }`}
             type="text"
             name=""
-            id={value}
-            defaultValue={value}
+            id={id}
+            value={value}
             disabled={!isEdit}
+            ref={inputRef}
+            onChange={e => {
+              value = e.target.value;
+            }}
           />
         </label>
         <Button
