e9d25f145f6b28762729bb8d9407efea878f8d40
ppotatoG <thgus2270@gmail.com>
2023-10-23 00:04:39 +0900

    Add: toast constants

---
+++ src/constants/toast/USERS_MESSAGE.ts
@@ -0,0 +1,10 @@
+export const USERS_MESSAGE = {
+  SEARCH: {
+    SUCCESS: 'users search success',
+    ERROR: 'users search error',
+  },
+  DETAIL: {
+    PASSWORD_RESET_SUCCESS: 'users password reset success',
+    EDIT_SUCCESS: 'users detail edit success',
+  },
+};
