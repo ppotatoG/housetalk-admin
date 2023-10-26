f219d11f8576b86327c980c5dcc66920897f54b7
ppotatoG <thgus2270@gmail.com>
2023-10-26 21:35:05 +0900

    Feat: provider 내부에 modal component 추가

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,13 +1,39 @@
-'use client';
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 
+import Modal from '@/component/Modal';
 import store from '@/store';
 
 interface ProvidersProps {
   children: React.ReactNode;
 }
 
+const ModalContainer: React.FC = () => {
+  const modals = useSelector((state: RootStateType) => state.modal);
+
+  if (modals.length === 0) return <></>;
+
+  return (
+    <>
+      {modals.map(modal => (
+        <Modal
+          key={modal.id}
+          title={modal.title}
+          description={modal.description}
+          type={modal.type}
+          id={modal.id}
+          callbackButton={modal.callbackButton}
+        />
+      ))}
+    </>
+  );
+};
+
 export function Providers({ children }: ProvidersProps) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      {children}
+      <ModalContainer />
+    </Provider>
+  );
 }
